f49475a7342ecf221c3b63ff1e42bc26cc630302
Felix <felavalos@gmail.com>
2025-02-09 17:21:22 -0600

    File system
    Create new pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login';
+import './styles/gobal.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
